c11438c7e84e90fafc17d986c8df0ddda16b3382
Knckd <thafisher@yahoo.com>
2024-10-06 04:17:02 -0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,11 +2,8 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   telegramId: { type: String, unique: true },
-  username: { type: String, unique: true },
-  password: { type: String },
+  referralCode: { type: String, unique: true },
   referrals: { type: Number, default: 0 },
-  referredBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null },
-  signupIPs: [{ type: String }],
 });
 
 module.exports = mongoose.model('User', userSchema);
