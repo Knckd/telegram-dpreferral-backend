5a0d664ee3f14250c9fdaec1769351766c8d3fb9
Knckd <thafisher@yahoo.com>
2024-10-06 01:48:18 -0700

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  telegramId: { type: String, unique: true },
+  referralCode: { type: String, unique: true },
+  referrals: { type: Number, default: 0 },
+});
+
+module.exports = mongoose.model('User', userSchema);
