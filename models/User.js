8be31e0865e133f8d4aee07b9d071a9f961a212a
Knckd <thafisher@yahoo.com>
2024-10-28 21:07:40 -0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,15 +2,12 @@
 
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema(
-  {
-    telegramId: { type: String, required: true, unique: true },
-    telegramUsername: { type: String, required: true }, // Removed unique constraint
-    referralCode: { type: String, required: true, unique: true },
-    referrals: { type: Number, default: 0 },
-    hasClaimed: { type: Boolean, default: false }, // Track if tokens have been claimed
-  },
-  { timestamps: true }
-);
+const userSchema = new mongoose.Schema({
+  telegramId: { type: String, required: true, unique: true },
+  telegramUsername: { type: String, required: true, unique: true },
+  referralCode: { type: String, required: true, unique: true },
+  referrals: { type: Number, default: 0 },
+  hasClaimed: { type: Boolean, default: false }, // Track if tokens have been claimed
+}, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
