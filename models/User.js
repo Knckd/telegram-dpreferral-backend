a5bbeb3e1e2780fa859ac437dcc11bcb3dd39fbf
Knckd <thafisher@yahoo.com>
2024-11-07 22:44:07 -0800

    Update User.js

--- models/User.js
+++ models/User.js
@@ -5,10 +5,11 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema(
   {
     telegramId: { type: String, required: true, unique: true },
-    telegramUsername: { type: String, required: true }, // Removed unique constraint
+    telegramUsername: { type: String, required: true },
     referralCode: { type: String, required: true, unique: true },
     referrals: { type: Number, default: 0 },
     hasClaimed: { type: Boolean, default: false }, // Track if tokens have been claimed
+    referredBy: { type: String }, // New field to track who referred the user
   },
   { timestamps: true }
 );
